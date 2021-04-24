85d0e70f2d5af22ed3242f9a3f439b7ce1768c32
Wellington Araujo <welles.araujo@hotmail.com>
2021-04-24 15:20:11 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {  Component } from 'react';
+import Cronometro from './assets/cronometro.png'
+import './style.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            number:0,
+            buttonLabel:'INICIAR'
+        }
+        this.timer=null;
+        this.playPause=this.playPause.bind(this);
+        this.zerar=this.zerar.bind(this);
+    }
+
+    playPause(){
+        if(!this.timer){
+            this.timer=setInterval(()=>{
+                let newState = this.state;
+                newState.number+=0.1;
+                newState.buttonLabel='PAUSAR'
+                this.setState(newState);
+            },100)
+        } else {
+            clearInterval(this.timer);
+            this.setState({buttonLabel:'CONTINUAR'})
+            this.timer=null;
+        }
+    }
+    zerar(){
+        const initialState={
+            number:0,
+            buttonLabel:'INCIAR'
+        }
+        clearInterval(this.timer);
+        this.timer=null;
+        this.setState(initialState)
+    }
+
+    render(){
+        return(
+            <div className='container'>
+                <img className='img' src={Cronometro}/>
+                <a className='timer'>{this.state.number.toFixed(1)}</a>
+                <div className='areaBtn'>
+                    <a className='botao' onClick={this.playPause}>{this.state.buttonLabel}</a>
+                    <a className='botao' onClick={this.zerar}>ZERAR</a>
+                </div>
+            </div>
+        )
+    }
 }
-
 export default App;
